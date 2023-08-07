c1b7c63e03b5116ba722a3645dbe65f0a61260b4
tjgtll <volotji@gmail.com>
2023-08-07 22:26:40 +0300

    feat: init logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (arguments.length == 0) return false;
+  let a = [];
+  for (let i = 0; i < str.length; i++) {
+    a.push(str[i]);
+    if (a.length > 0) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if (
+          a[a.length - 1] == bracketsConfig[j][1] &&
+          a[a.length - 2] == bracketsConfig[j][0]
+        ) {
+          a.pop();
+          a.pop();
+        }
+      }
+    }
+  }
+  if (a.length == 0) return true;
+  return false;
+};
